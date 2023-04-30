77766e48c39dcecbbdd8eac77e8701930b845bf2
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2023-04-30 19:13:00 -0400

    getElementsByClassName
    more DOM practice

--- Browser-environment/app.js
+++ Browser-environment/app.js
@@ -5,6 +5,13 @@ const headline = document.getElementById("headline");
 
 const items = document.getElementsByTagName("li");
 
+const highlights = document.getElementsByClassName("highlight");
+
+for ( const highlight of highlights ) {
+    highlight.style.background = "cornsilk";
+}
+
+
 for (let i = 0; i < items.length; i++) {
     items[i].style.color = 'orchid';
 }
@@ -12,4 +19,5 @@ for (let i = 0; i < items.length; i++) {
 
 btnUpdate.addEventListener("click", () => {
     headline.style.border = 'solid 2px red';
+   
 });
