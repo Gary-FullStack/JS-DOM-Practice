45f60f2f5404ef2d72d17832bbd124f134b8214a
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2023-04-30 20:12:07 -0400

    textContent and innerHTML
    dommin

---
+++ Getting-a-handle-on-the-dom/app.js
@@ -0,0 +1,14 @@
+const btnUpdate = document.querySelector('.btn-main');
+
+
+
+
+
+
+btnUpdate.addEventListener('click', () => {
+  const input = document.querySelector(".input-main");
+  const headline = document.getElementById('headline'); 
+
+  headline.textContent = input.value;
+  input.value = ("")
+});
