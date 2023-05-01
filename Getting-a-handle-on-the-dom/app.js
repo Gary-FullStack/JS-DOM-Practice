243478daabc9e45d5e035e88b8775efd785f0827
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2023-05-01 14:41:29 -0400

    more dom
    dommin

--- Getting-a-handle-on-the-dom/app.js
+++ Getting-a-handle-on-the-dom/app.js
@@ -1,14 +1,11 @@
 const btnUpdate = document.querySelector('.btn-main');
 
 
-
-
-
-
 btnUpdate.addEventListener('click', () => {
   const input = document.querySelector(".input-main");
   const headline = document.getElementById('headline'); 
 
+  headline.className = "grow";
   headline.textContent = input.value;
   input.value = ("")
 });
